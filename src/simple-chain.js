ab37becb4d9972b55fdc7592cc8124ff0d9bf6a8
Julia Norwill <julianorwill@gmail.com>
2023-04-17 17:55:42 +0400

    feat: add chainMaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(`( ${value} )`)
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (!this.chain[position]) {
+      throw new Error('You can\'t remove incorrect link!')
+    }
+    this.chain.splice(position - 1, 1)
+    return this
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse()
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const arr = this.chain.slice()
+    this.chain = []
+    return arr.join('~~')
   }
 };
 
